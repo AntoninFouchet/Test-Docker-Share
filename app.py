846c1566b7bff0ce44531e348ef47e853f2b6c26
AntoninFouchet <antoninfouchet@gmail.com>
2026-01-13 16:55:11 +0100

    Tests-Attaque

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-print("passage dans app.py (volume)")
+from datetime import datetime
+import time
+print("Le docker fonctionne")
 
 data_path = "/app/data/test_volume.txt"
+
+
+time.sleep(1000)
+now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+with open(data_path, "a") as f:
+    f.write(f"Passage de app.py le {now}\n")
+
 
